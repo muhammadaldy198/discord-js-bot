021528715a200cc408ef87c751158e639e27f334
muhammadaldy198 <muhammadaldy198@gmail.com>
2026-08-20 15:44:31 +0700

    Update mongoose.js

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -20,13 +20,14 @@ module.exports = {
   },
 
   schemas: {
-    Giveaways: require("./schemas/Giveaways"),
-    Guild: require("./schemas/Guild"),
-    Member: require("./schemas/Member"),
-    ReactionRoles: require("./schemas/ReactionRoles").model,
-    ModLog: require("./schemas/ModLog").model,
-    TranslateLog: require("./schemas/TranslateLog").model,
-    User: require("./schemas/User"),
-    Suggestions: require("./schemas/Suggestions").model,
-  },
+  Giveaways: require("./schemas/Giveaways"),
+  Guild: require("./schemas/Guild"),
+  Member: require("./schemas/Member"),
+  ReactionRoles: require("./schemas/ReactionRoles").model,
+  ModLog: require("./schemas/ModLog").model,
+  TranslateLog: require("./schemas/TranslateLog").model,
+  User: require("./schemas/User"),
+  Suggestions: require("./schemas/Suggestions").model,
+  Sticky: require("./schemas/Sticky"),
+ },
 };
